2175198d100d75b7405137ad03de7ba9e6ac5f97
deaelsyifana <deaelsyifanaqurratuain@gmail.com>
2026-03-05 08:16:44 +0700

    header change

--- data.php
+++ data.php
@@ -8,8 +8,7 @@
     <div class="container">
         <h2 style="color: white; margin-bottom: 20px; font-family: 'Times New Roman', serif; text-transform: uppercase; letter-spacing: 2px;">Rentetan Kegiatan PKA</h2>
         <p style="color: rgba(255,255,255,0.7); font-family: 'Times New Roman', serif;">
-            Sabtu, 14 Februari 2026 <br>
-            Gedung Putih, Jakarta
+            Sabtu, 14 oktober 2026 <br>
         </p>
     </div>
 </section>
